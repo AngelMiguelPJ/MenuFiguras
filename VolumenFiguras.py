4470863a9d067db89516a90b6905defa61b1fe62
David-PC\David <bryan.guerrero01@epn.edu.ec>
2018-05-02 14:55:05 -0500

    Implementación volumen de esfera

--- VolumenFiguras.py
+++ VolumenFiguras.py
@@ -1,16 +1,24 @@
 # definimos la funcion cubo
 import math
+
 def VolumenCubo():
     print("\n\t\tCubo")
     lado = (float)(input("Introduzca un lado del cubo: "))
     volumencubo = math.pow(lado,3)
     print("El volumen del cubo es: ",'%.2f' % volumencubo , "m^3")
+
+def VolumenEsfera():
+    print("\n\t\tEsfera")
+    radio = (float)(input("Introduzca el radio de la esfera: "))
+    volumenesfera = (4/3)*math.pi*math.pow(radio,3)
+    print(f"El volumen de la esfera es: ",volumenesfera,"m^3")
+
 # Definimos la funcion menu con las opciones
 
 def menu():
     print("\nMenu\n"
           +"1.VolumenCubo\n"
-          +"2.Volumen----\n"
+          +"2.VolumenEsfera\n"
           +"3.Volumen----\n"
           +"4.Salir\n")
 
@@ -29,9 +37,15 @@ def MenuFiguras():
         if opciones == 1:
         # Llamamos a la funcion cubo para realizar la operacion del volumen
             VolumenCubo();
+        if opciones == 2:
+        # Calculo del volumen de la esfera
+            VolumenEsfera();
         elif opciones == 4:
             print("Gracias por usar el programa")
         else:
             print("Ingrese una opcion valida")
 
-MenuFiguras()
+#Funcion Principal
+
+if __name__ == '__main__':
+    MenuFiguras()
